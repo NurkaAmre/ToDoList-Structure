288a08c0958d780420f8d9a42e075076ba050697
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-10-07 15:33:14 +0600

    Add remove clear functions

--- src/index.js
+++ src/index.js
@@ -1,48 +1,102 @@
 import './style.css';
+import store from './modules/store';
+import displayTodo from './modules/function.js';
+import formClear from './modules/cleartask.js';
 
-const taskInput = document.querySelector('.task-input input');
-let taskBox = document.querySelector('.task-box');
+const form = document.querySelector('.form');
 
-//Get localStorage todo-list 
-let todos = localStorage.getItem('todo-list') ? JSON.parse(localStorage.getItem('todo-list')) : [];
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = Object.fromEntries(new FormData(e.target));
+  store(input.task);
+  displayTodo();
+});
 
+const clearBtn = document.querySelector('.clear-btn');
 
-function displayTodo() {
-  console.log(todos)
-  let li = '';
-    todos.forEach((todo, id) => {
-      li += `
-        <li class="task">
-                <label for="${id}">
-                  <input onclick="updateStatus(this)" type="checkbox" id="${id}">
-                  <p>${todo.name}</p>
-                </label>
-                <div class="settings">
-                  <button class="three-dot">&#10247</button>
-                  <button class="task-menu">&#x1F5D1</button>
-                </div>
-          </li>
-          <hr>
-        `;
-    });
-  taskBox.innerHTML = li;
-}
+clearBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  formClear();
+  displayTodo();
+});
+
 displayTodo();
 
-function updateStatus(selectedTask) {
-  console.log(selectedTask)
-}
 
-taskInput.addEventListener('keyup', e => {
-  let userTask = taskInput.value.trim();
-  if(e.key == "Enter" && userTask) {
-    if(!todos) {
-      todos = [];
-    }
-    taskInput.value = '';
-    let taskInfo = {name: userTask};
-    todos.push(taskInfo); // adds new task to todos
-    localStorage.setItem('todo-list', JSON.stringify(todos));
-    displayTodo();
-  }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Get localStorage todo-list 
+// let todos = localStorage.getItem('todo-list') ? JSON.parse(localStorage.getItem('todo-list')) : [];
+
+
+// function displayTodo() {
+//   let li = '';
+//     todos.forEach((todo, id) => {
+//       li += `
+//         <li class="task">
+//                 <label for="${id}">
+//                   <input onclick="updateStatus(this)" type="checkbox" id="${id}">
+//                   <p>${todo.name}</p>
+//                 </label>
+//                 <div class="settings">
+//                   <button class="three-dot">&#10247</button>
+//                   <button class="task-menu">&#x1F5D1</button>
+//                 </div>
+//           </li>
+//           <hr>
+//         `;
+//     });
+//   taskBox.innerHTML = li;
+// }
+// displayTodo();
+
+// function updateStatus(selectedTask) {
+  
+// }
+
+// taskInput.addEventListener('keyup', e => {
+//   let userTask = taskInput.value.trim();
+//   if(e.key == "Enter" && userTask) {
+//     if(!todos) {
+//       todos = [];
+//     }
+//     taskInput.value = '';
+//     let taskInfo = {name: userTask, status: "pending"};
+//     todos.push(taskInfo); // adds new task to todos
+//     localStorage.setItem('todo-list', JSON.stringify(todos));
+//     displayTodo();
+//   }
+// });
---
+++ src/modules/function.js
@@ -0,0 +1,87 @@
+export default function displayTodo() {
+    const taskList = document.querySelector('.task-list');
+  
+    /* Display Tasks */
+    let parsedArr = JSON.parse(localStorage.getItem('taskArr')) || [];
+  
+    taskList.innerHTML = '';
+    parsedArr.map((task) => {
+      const taskItem = document.createElement('li');
+      let box;
+      let styling;
+      if (task.completed) {
+        box = 'checked';
+        styling = 'line-through';
+      } else {
+        box = '';
+        styling = 'none';
+      }
+      taskItem.innerHTML = `<form class="task-form b-bottom box">
+        <input name="completed" type="checkbox" ${box} class="checkbox">
+        <textarea name="description" rows="1" class="task-text full" style="text-decoration:${styling}">${task.description}</textarea>
+        <button type="button" class="delete btn">
+        &#x1F5D1
+        </button>
+        <button type="submit" class="update btn">
+        &#10247
+        </button>
+        </form>`;
+      taskList.appendChild(taskItem);
+  
+      const deleteBtn = taskItem.querySelector('.delete');
+      const updateBtn = taskItem.querySelector('.update');
+      const updateText = taskItem.querySelector('.task-text');
+  
+      updateBtn.style.display = 'none';
+  
+      /* Edit task */
+      updateText.addEventListener('click', (e) => {
+        e.preventDefault();
+        updateBtn.style.display = 'block';
+        deleteBtn.style.display = 'none';
+        updateText.style.backgroundColor = '#f4f4f4';
+      });
+  
+      const taskForm = taskItem.querySelector('.task-form');
+      taskForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = Object.fromEntries(
+          new FormData(e.target),
+        );
+        task.description = input.description;
+        localStorage.setItem('taskArr', JSON.stringify(parsedArr));
+        updateBtn.style.display = 'none';
+        deleteBtn.style.display = 'block';
+        updateText.style.backgroundColor = '#fff';
+      });
+  
+      /* Delete task */
+      deleteBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        let temp = parsedArr.filter((item) => item !== task);
+        parsedArr = temp;
+        temp = parsedArr.map((item) => {
+          item.index = parsedArr.indexOf(item) + 1;
+          return item;
+        });
+        parsedArr = temp;
+        localStorage.setItem('taskArr', JSON.stringify(parsedArr));
+        taskList.removeChild(taskItem);
+      });
+  
+      /* Checkbox status   */
+      const checkbox = taskItem.querySelector('.checkbox');
+      checkbox.addEventListener('change', () => {
+        task.completed = checkbox.checked;
+        localStorage.setItem('taskArr', JSON.stringify(parsedArr));
+  
+        if (task.completed) {
+          updateText.style.textDecoration = 'line-through';
+        } else {
+          updateText.style.textDecoration = 'none';
+        }
+      });
+  
+      return taskList;
+    });
+  }
